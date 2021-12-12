69eea783f76fdda42d42062066edbe1dab0c5975
bora <adbr.dev@gmail.com>
2021-12-12 18:57:15 +0900

    feat: new flutter project

---
+++ lib/page/home_page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('btn'),
+          ),
+          const Center(child: Text('hi')),
+        ],
+      ),
+    );
+  }
+}
