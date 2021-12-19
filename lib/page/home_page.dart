bf7fa01a522ee32a1a58cf4ab052b6a8fd16f996
bora <adbr.dev@gmail.com>
2021-12-19 15:14:19 +0900

    feat: add custom toast

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -9,6 +9,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late FToast fToast;
+
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,21 +28,50 @@ class _HomePageState extends State<HomePage> {
         children: [
           ElevatedButton(
             onPressed: () {
-              Fluttertoast.showToast(
-                msg: "toast",
-                toastLength: Toast.LENGTH_LONG,
-                gravity: ToastGravity.CENTER,
-                timeInSecForIosWeb: 1,
-                backgroundColor: Colors.blueGrey,
-                textColor: Colors.white,
-                fontSize: 20.0,
-              );
+              _showToast();
             },
             child: const Text('btn'),
           ),
           const Center(child: Text('hi')),
         ],
       ),
+    );
+  }
+
+  void _showToast() {
+    Widget toast = Container(
+      padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        color: Colors.greenAccent,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+          Icon(Icons.check),
+          SizedBox(width: 12.0),
+          Text("This is a Custom Toast"),
+        ],
+      ),
+    );
+
+    fToast.showToast(
+      child: toast,
+      gravity: ToastGravity.BOTTOM,
+      toastDuration: const Duration(seconds: 1),
+    );
+
+    // Custom Toast Position
+    fToast.showToast(
+      child: toast,
+      toastDuration: const Duration(seconds: 10),
+      positionedToastBuilder: (context, child) {
+        return Positioned(
+          child: child,
+          top: 100.0,
+          right: 50,
+        );
+      },
     );
   }
 }
