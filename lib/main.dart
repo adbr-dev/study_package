63c5f8665d2a5a51c6c24d24f9a629966aaf16ab
bora <adbr.dev@gmail.com>
2021-12-22 14:42:11 +0900

    feat: add init notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:study_package/page/home_page.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  _initNotiSetting();
+
   runApp(const MyApp());
+}
+
+final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+void _initNotiSetting() async {
+  const initializationSettingsAndroid =
+      AndroidInitializationSettings('@mipmap/ic_launcher');
+
+  const initializationSettingsIOS = IOSInitializationSettings(
+    requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+  );
+
+  const initializationSettings = InitializationSettings(
+    android: initializationSettingsAndroid,
+    iOS: initializationSettingsIOS,
+  );
+
+  final result = await flutterLocalNotificationsPlugin.initialize(
+    initializationSettings,
+  );
+
+  print(result);
 }
 
 class MyApp extends StatelessWidget {
