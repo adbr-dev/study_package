236aac83dbc128c45e6652d46527a04ed0007df8
bora <adbr.dev@gmail.com>
2021-12-22 15:30:56 +0900

    feat: add timezone

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:study_package/page/home_page.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
+import 'package:timezone/timezone.dart' as tz;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,6 +15,10 @@ void main() {
 final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 void _initNotiSetting() async {
+  tz.initializeTimeZones();
+  final timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
+  tz.setLocalLocation(tz.getLocation(timeZoneName));
+
   const initializationSettingsAndroid =
       AndroidInitializationSettings('@mipmap/ic_launcher');
 
